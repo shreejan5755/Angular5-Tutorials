765d603dab6b46fe596dd19aad38c7a43da9b797
Shreejan Shrestha <shreejan5755shrestha@gmail.com>
2018-10-04 18:03:33 -0500

    Detecting changes in the input using ngOnChanges Life cycle hook

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,5 +1,5 @@
 import { Employee } from './../models/employee.model';
-import { Input } from '@angular/core';
+import { Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
 
   @Input() employee: Employee;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    const previousEmployee = <Employee>changes.employee.previousValue;
+    const currentEmployee = <Employee>changes.employee.currentValue;
+
+    console.log('Previous : '+ (previousEmployee ? previousEmployee.name : 'NULL'));
+    console.log('Current : '+ currentEmployee.name);
   }
 
 }
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -9,10 +9,29 @@ import { EmployeeService } from './employee.service';
 })
 export class ListEmployeesComponent implements OnInit {
   employees: Employee[];
+
+  // variable to hold the current employee data
+  employeeToDisplay: Employee;
+
+  // private variable to hold the current employee index
+  private arrayIndex = 1;
+
   constructor( private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
+    this.employeeToDisplay = this.employees[0];
+  }
+
+  nextEmployee(): void{
+    if(this.arrayIndex <= 2){
+      this.employeeToDisplay = this.employees[this.arrayIndex];
+      this.arrayIndex++;
+    }
+    else{
+      this.employeeToDisplay = this.employees[0];
+      this.arrayIndex = 1;
+    }
   }
 
 }
