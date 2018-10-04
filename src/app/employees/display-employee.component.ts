2888a70e2a8cf3d0858f81be5bbd5e528ea801c8
Shreejan Shrestha <shreejan5755shrestha@gmail.com>
2018-10-04 18:12:23 -0500

    35 using the Property setter to intersept and react to the property changes

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -7,20 +7,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges {
+export class DisplayEmployeeComponent implements OnInit{
 
-  @Input() employee: Employee;
+  //constructing a private field to get and set employee
+  private _employee: Employee;
+
+  //constructing the getter and setter for the input
+  @Input()
+  set employee(val : Employee){
+    console.log('Previous : '+ (this._employee ? this._employee.name : 'NULL'));
+    console.log('Current : '+ val.name);
+    this._employee = val;
+  }
+  get employee(): Employee{
+    return this._employee;
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  ngOnChanges(changes: SimpleChanges){
-    const previousEmployee = <Employee>changes.employee.previousValue;
-    const currentEmployee = <Employee>changes.employee.currentValue;
+  // ngOnChanges(changes: SimpleChanges){
+  //   const previousEmployee = <Employee>changes.employee.previousValue;
+  //   const currentEmployee = <Employee>changes.employee.currentValue;
 
-    console.log('Previous : '+ (previousEmployee ? previousEmployee.name : 'NULL'));
-    console.log('Current : '+ currentEmployee.name);
-  }
+  //   console.log('Previous : '+ (previousEmployee ? previousEmployee.name : 'NULL'));
+  //   console.log('Current : '+ currentEmployee.name);
+  // }
 
 }
