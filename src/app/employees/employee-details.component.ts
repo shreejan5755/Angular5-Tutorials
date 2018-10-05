e363650dd0521218d59190d6b05f2016481c3aef
Shreejan Shrestha <shreejan5755shrestha@gmail.com>
2018-10-05 16:48:09 -0500

    reading route parameter using Obeservable

--- src/app/employees/employee-details.component.ts
+++ src/app/employees/employee-details.component.ts
@@ -1,6 +1,6 @@
 import { Employee } from './../models/employee.model';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EmployeeService } from './employee.service';
 
 @Component({
@@ -12,12 +12,30 @@ export class EmployeeDetailsComponent implements OnInit {
 
   employee: Employee;
 
+  //using the id field to keep track of the employeeId
+  private _id: number;
+
   constructor(private _route: ActivatedRoute,
-    private _employeeService: EmployeeService){ }
+    private _employeeService: EmployeeService,
+    private _router: Router){ }
 
   ngOnInit() {
-    const id = +this._route.snapshot.paramMap.get('id');
-    this.employee = this._employeeService.getEmployee(id);
+    //this._id = +this._route.snapshot.paramMap.get('id');//+this._route.snapshot.param['id']
+    //observable approach
+    this._route.paramMap.subscribe(params =>{ 
+      this._id = +params.get('id');
+      this.employee = this._employeeService.getEmployee(this._id);
+    });    
+  }
+
+  //to move to next employee details
+  viewNextEmployee(){
+    if(this._id < 3){
+    this._id = this._id + 1;    
+    }else{
+      this._id = 1;
+    }
+    this._router.navigate(['/employees', this._id]);
   }
 
 }
