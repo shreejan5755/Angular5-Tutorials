d63d08847e11200f4929246a0d0835e8dccda4b4
Shreejan Shrestha <shreejan5755shrestha@gmail.com>
2018-10-05 12:28:37 -0500

    Using template variable to get the data from child to parent component

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -11,17 +11,13 @@ export class DisplayEmployeeComponent implements OnInit {
 
   @Input() employee: Employee;
 
-  // Creating output property to pass the employee name from child to parent component.
-  // Child passes data to parent thorugh an event so the type of output property is defined as EventEmitter
-  @Output() notify: EventEmitter<Employee> = new EventEmitter<Employee>();
-
   constructor() { }
 
   ngOnInit() {
   }
 
-  handleClick() {
-    this.notify.emit(this.employee);
+  getEmployeeNameAndGender(): string {
+    return this.employee.name + ' ' + this.employee.gender;
   }
 
 }
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -10,18 +10,11 @@ import { EmployeeService } from './employee.service';
 export class ListEmployeesComponent implements OnInit {
   
   employees: Employee[];
-  //to keep track of the data from child
-  dataFromChild: Employee;
   
   constructor( private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();    
   }
-
-  // method to handle the output event emitted by the child component
-  handleNotify(eventData: Employee) {
-    this.dataFromChild = eventData;
-  } 
 
 }
