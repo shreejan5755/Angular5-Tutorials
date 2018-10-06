9a21713e8778a184af6282aaad5dee1a5b1c76fa
Shreejan Shrestha <shreejan5755shrestha@gmail.com>
2018-10-06 13:10:39 -0500

    including buttons in the employee list and implementing the view button to show the details for the individual user

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from './../models/employee.model';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
@@ -11,15 +11,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class DisplayEmployeeComponent implements OnInit {
 
   @Input() employee: Employee;
+  @Input() searchTerm: string;
+  
   private selectedEmployeeId: number;
   
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
     this.selectedEmployeeId = +this._route.snapshot.paramMap.get('id');
   }
 
- 
+  viewEmployee(){
+    this._router.navigate(['/employees', this.employee.id], {
+      queryParams: { 'searchTerm': this.searchTerm}
+    });
+  }
   
 }
